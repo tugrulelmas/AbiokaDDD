3300010b81345e837620765ce11d165a496f9bae
tugrul <tugrulelmas@gmail.com>
2016-03-22 16:23:03 +0200

    added new files

---
+++ AbiokaDDD.ApplicationService/Messaging/AddCommentResponse.cs
@@ -0,0 +1,9 @@
+﻿using AbiokaDDD.ApplicationService.DTOs;
+
+namespace AbiokaDDD.ApplicationService.Messaging
+{
+    public class AddCommentResponse : ServiceResponseBase
+    {
+        public CommentDTO Comment { get; set; }
+    }
+}
---
+++ AbiokaDDD.Repository.MongoDB/DatabaseObjects/CommentMongoDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace AbiokaDDD.Repository.MongoDB.DatabaseObjects
+{
+    internal class CommentMongoDB : MongoValueObject
+    {
+        public Guid CardId { get; set; }
+
+        public string Text { get; set; }
+    }
+}
